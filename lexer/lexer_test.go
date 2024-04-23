6ba5f5f42aed8a25765bc23c097622546b92a65e
1200966 <gaspardiogo55@gmail.com>
2024-04-23 12:16:50 +0100

    test: add test case for emoji identifier and float data type

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -13,6 +13,14 @@ let ten = 10;
 let add = fn(x, y) {
   x + y;
 };
+
+let lessThanOrEqualTo = fn(x, y) {
+  x <= y;
+};
+
+let result = add(five, ten);
+
+let 🤖 = 3.14;
 `
 
 	expectedFilename := "test.go"
@@ -29,11 +37,13 @@ let add = fn(x, y) {
 		{token.ASSIGN, "=", expectedFilename, 1, 9},
 		{token.INT, "5", expectedFilename, 1, 11},
 		{token.TERMINATOR, ";", expectedFilename, 1, 12},
+		
 		{token.LET, "let", expectedFilename, 2, 0},
 		{token.IDENTIFIER, "ten", expectedFilename, 2, 4},
 		{token.ASSIGN, "=", expectedFilename, 2, 8},
 		{token.INT, "10", expectedFilename, 2, 10},
 		{token.TERMINATOR, ";", expectedFilename, 2, 12},
+
 		{token.LET, "let", expectedFilename, 4, 0},
 		{token.IDENTIFIER, "add", expectedFilename, 4, 4},
 		{token.ASSIGN, "=", expectedFilename, 4, 8},
@@ -50,7 +60,40 @@ let add = fn(x, y) {
 		{token.TERMINATOR, ";", expectedFilename, 5, 7},
 		{token.RBRACE, "}", expectedFilename, 6, 0},
 		{token.TERMINATOR, ";", expectedFilename, 6, 1},
-		
+
+		{token.LET, "let", expectedFilename, 8, 0},
+		{token.IDENTIFIER, "lessThanOrEqualTo", expectedFilename, 8, 4},
+		{token.ASSIGN, "=", expectedFilename, 8, 22},
+		{token.FUNCTION, "fn", expectedFilename, 8, 24},
+		{token.LPAREN, "(", expectedFilename, 8, 26},
+		{token.IDENTIFIER, "x", expectedFilename, 8, 27},
+		{token.COMMA, ",", expectedFilename, 8, 28},
+		{token.IDENTIFIER, "y", expectedFilename, 8, 30},
+		{token.RPAREN, ")", expectedFilename, 8, 31},
+		{token.LBRACE, "{", expectedFilename, 8, 33},
+		{token.IDENTIFIER, "x", expectedFilename, 9, 2},
+		{token.LTE, "<=", expectedFilename, 9, 4},
+		{token.IDENTIFIER, "y", expectedFilename, 9, 7},
+		{token.TERMINATOR, ";", expectedFilename, 9, 8},
+		{token.RBRACE, "}", expectedFilename, 10, 0},
+		{token.TERMINATOR, ";", expectedFilename, 10, 1},
+
+		{token.LET, "let", expectedFilename, 12, 0},
+		{token.IDENTIFIER, "result", expectedFilename, 12, 4},
+		{token.ASSIGN, "=", expectedFilename, 12, 11},
+		{token.IDENTIFIER, "add", expectedFilename, 12, 13},
+		{token.LPAREN, "(", expectedFilename, 12, 16},
+		{token.IDENTIFIER, "five", expectedFilename, 12, 17},
+		{token.COMMA, ",", expectedFilename, 12, 21},
+		{token.IDENTIFIER, "ten", expectedFilename, 12, 23},
+		{token.RPAREN, ")", expectedFilename, 12, 26},
+		{token.TERMINATOR, ";", expectedFilename, 12, 27},
+
+		{token.LET, "let", expectedFilename, 14, 0},
+		{token.IDENTIFIER, "🤖", expectedFilename, 14, 4},
+		{token.ASSIGN, "=", expectedFilename, 14, 6},
+		{token.FLOAT, "3.14", expectedFilename, 14, 8},
+		{token.TERMINATOR, ";", expectedFilename, 14, 12},
 	}
 
 	l := New("test.go", input)
