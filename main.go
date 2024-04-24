5ba8f7958db26d672a7229465b982a9b5b361478
1200966 <gaspardiogo55@gmail.com>
2024-04-24 20:55:52 +0100

    feat: add basic implementation of REPL

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/diogo-gaspar23/monkey2/repl"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	currUser, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", currUser.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
